b3f045ff472f24b1af4a2b11051c73473bdb9a3c
Gaurav Purohit <grajpurohittt@gmail.com>
2025-06-30 22:25:49 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://virtualassistant-frontend-o1og.onrender.com"
+    ],
     credentials:true
 }))
 const port=process.env.PORT
